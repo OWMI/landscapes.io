5a6cf14011436f63b03464873cd9640bd4368ba8
Matthew Fincher <matthewfincher@gmail.com>
2017-02-07 15:45:36 -0500

    default admin toggled on group add

--- app/views/groups/CreateGroup.js
+++ app/views/groups/CreateGroup.js
@@ -279,10 +279,10 @@ class CreateGroup extends Component {
                                             <TableRowColumn>{row.lastName}, {row.firstName} </TableRowColumn>
                                             <TableRowColumn>{row.email}</TableRowColumn>
                                             <TableRowColumn>
-                                              <Toggle toggled={row.isAdmin} onToggle={() => (
+                                              <Toggle toggled={row.isAdmin || (row.role === 'admin')} onToggle={() => (
                                                   this.state.stateUsers[index].isAdmin = !this.state.stateUsers[index].isAdmin,
                                                   this.setState({stateUsers: [...this.state.stateUsers]})
-                                                )} />
+                                                )} disabled={row.role === 'admin'}/>
                                             </TableRowColumn>
                                         </TableRow>
                                     ))
@@ -508,6 +508,9 @@ class CreateGroup extends Component {
         if (this.state.selectedUserRows) {
             console.log('theres landscapes');
             for (var i = 0; i < this.state.selectedUserRows.length; i++) {
+                if(this.state.users[this.state.selectedUserRows[i]].role === 'admin'){
+                  this.state.users[this.state.selectedUserRows[i]].isAdmin = true;
+                }
                 groupToCreate.users.push({
                     userId: this.state.stateUsers[this.state.selectedUserRows[i]]._id,
                     isAdmin: this.state.stateUsers[this.state.selectedUserRows[i]].isAdmin || false
