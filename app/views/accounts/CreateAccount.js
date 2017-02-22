fc12e9cb575d5a13431abee7aaf30f894176c19e
Matthew Fincher <matthewfincher@gmail.com>
2017-02-22 11:52:54 -0500

    consistency in account create

--- app/views/accounts/CreateAccount.js
+++ app/views/accounts/CreateAccount.js
@@ -117,9 +117,24 @@ class CreateAccount extends Component {
                                 <h4>New Account</h4>
                             </Col>
                             <Col xs={8}>
-                                <RaisedButton label='Save' onClick={this.handlesCreateClick}
+                              <Row>
+                                <Col xs={4}>
+
+                                </Col>
+                                <Col xs={4}>
+                                  <RaisedButton label='Save' onClick={this.handlesCreateClick}
+                                      style={{ float: 'right', margin: '30px 0px' }}
+                                      labelStyle={{ fontSize: '11px' }}/>
+                                </Col>
+                                <Col xs={4}>
+                                  <RaisedButton label='Cancel' primary={true} onClick={() => {
+                                      const {router} = this.context
+                                      router.push(`/accounts`)
+                                  }}
                                     style={{ float: 'right', margin: '30px 0px' }}
                                     labelStyle={{ fontSize: '11px' }}/>
+                                </Col>
+                              </Row>
                             </Col>
                         </Row>
                         <Card>
