31d2d4011d394efed1c329c3b4fa2a0346568335
Apurv Parikh <apurvparikh1@gmail.com>
2017-05-09 11:56:20 -0400

    fixed port issue

--- config_ip.sh
+++ config_ip.sh
@@ -3,7 +3,14 @@ echo "Configuring landscapes..."
 
 # set environment variables
 DNS_NAME=$(env | grep DNS_NAME | grep -oe '[^=]*$');
-IP=${DNS_NAME:=127.0.0.1}
+SERVER_PORT=$(env | grep DNS_PORT | grep -oe '[^=]*$');
+
+# fallback if environment variables not set
+SERVER_IP=${DNS_NAME:=0.0.0.0}
+SERVER_PORT=${DNS_PORT:=8080}
+
+echo "using DNS_NAME " $SERVER_IP
+echo "using PORT " $SERVER_PORT
 
 # search and replace dns
-sed -i -e "s/\:\/\/0.0.0.0/\:\/\/${IP}/g" /opt/landscapes/dist/dist/assets/bundle.js
+sed -i -e "s/\:\/\/0.0.0.0:80/\:\/\/${SERVER_IP}\:${SERVER_PORT}/g" /opt/landscapes/dist/dist/assets/bundle.js
